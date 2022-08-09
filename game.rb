716438e491843303f8166bba05d179d368274f59
Joshua <mramos29@ucol.mx>
2022-08-09 17:08:04 -0500

    new_gen testing

--- game.rb
+++ game.rb
@@ -58,6 +58,34 @@ def count_neighbors(grid, row, col)
 end
 
 puts count_neighbors(grid, 0,0)
-puts grid[0][0]
+
+
+def new_gen(grid,row,col)
+	grid_2 = Array.new(row) { Array.new(col, '.') }
+	row.times do |i|
+		col.times do |j|
+			puts j
+			n = count_neighbors(grid, i, j)
+			cell = ""
+			if (grid[i][j] == "*" && n < 2)
+				cell = "."
+			
+			elsif (grid[i][j] == "*" && n > 3)
+				cell = "."
+			
+			elsif (grid[i][j] == "*" && n > 2)
+				cell = "*"
+			
+			elsif (grid[i][j] == "." && n == 3)
+				cell = "*"
+			end
+			grid_2[i][j] = cell
+		end
+	end
+	print_grid(grid_2)
+end
+
+# puts grid[0][0]
 
 print_grid(grid)
+new_gen(grid,row,col)
